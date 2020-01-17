79d19c68738c39ccc43d01a4a586daa0ea68d740
rkambal <59982115+rkambal@users.noreply.github.com>
2020-01-16 18:39:59 -0600

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,10 @@
+/*"Hello World", My first program
+Name: Reda Kambal
+Date: 2020/01/14
+*/
+#include <stdio.h>
+int main(void) {
+	printf("Hello Everyone! I'm Reda Kambal\n");
+	printf("\nMy Major is Computer Science");
+	return 0;
+}
